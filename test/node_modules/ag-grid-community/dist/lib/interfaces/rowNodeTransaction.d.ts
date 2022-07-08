7bea7ca768dd6eccf62590a2d1eddef9b149eead
Tim Markowski <tmarkowski@studenten.hs-bremerhaven.de>
2022-07-08 20:30:17 +0200

    delete

--- test/node_modules/ag-grid-community/dist/lib/interfaces/rowNodeTransaction.d.ts
+++
@@ -1,9 +0,0 @@
-import { RowNode } from "../entities/rowNode";
-export interface RowNodeTransaction<TData = any> {
-    /** Row nodes added */
-    add: RowNode<TData>[];
-    /** Row nodes removed */
-    remove: RowNode<TData>[];
-    /** Row nodes updated */
-    update: RowNode<TData>[];
-}
